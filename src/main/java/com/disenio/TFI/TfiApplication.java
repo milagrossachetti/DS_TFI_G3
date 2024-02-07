df0cf50fbe3849e772a5c06ecc444c3b77ee6efe
Milagros Sachetti <milagrossachett@gmail.com>
2024-02-07 13:38:25 -0300

    Se agregan las carpetas y las dependencias de lombok, jpa y mysql

---
+++ src/main/java/com/disenio/TFI/TfiApplication.java
@@ -0,0 +1,13 @@
+package com.disenio.TFI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfiApplication.class, args);
+	}
+
+}
